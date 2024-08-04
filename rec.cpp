004c682aaee355f077d5ef6c6a97b3e77abab405
saketbhatnagar1 <165547660+saketbhatnagar1@users.noreply.github.com>
2024-08-04 16:26:44 +0530

    recursion

--- rec.cpp
+++ rec.cpp
@@ -1,81 +1,116 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-//recursion is a type of function calling itself,function call may be direct or indirect
-//when a problem's solution relies on the same problems solution on a smaller scale
-//for example 2^3 = 2x2x2 => 2^n = 2x2^(n-1)
-//f(n) = 2 x f(n-1)
-//eg.2 5!=  5x4x3x2x1 == 5x4! => n! = nx(n-1)!
+// Basic recursion programs to explain working and callstack of recursion
 
+// recursion is a type of function calling itself,function call may be direct or indirect
+// when a problem's solution relies on the same problems solution on a smaller scale
+// for example 2^3 = 2x2x2 => 2^n = 2x2^(n-1)
+// f(n) = 2 x f(n-1)
+// eg.2 5!=  5x4x3x2x1 == 5x4! => n! = nx(n-1)!
 
-//tail recursion:
-//base case=>condition where recursion stops therefore base case in 2^n is 2^0
-//function body
-//recursive relation
+// tail recursion:
+// base case=>condition where recursion stops therefore base case in 2^n is 2^0
+// function body
+// recursive relation
 
+// head recursion:
+// base case=>condition where recursion stops therefore base case in 2^n is 2^0
+// recursive relation
+// function body
 
+double fact(int n)
+{
+    if (n == 0)
+    {
+        return 1; // base case me return likhna mandatory hai since it returns the answer to the function
+    }
+    else
+    {
+        return n * fact(n - 1);
+    }
 
-
-//head recursion:
-//base case=>condition where recursion stops therefore base case in 2^n is 2^0
-//recursive relation
-//function body
-
-
-
-double fact(int n){
-if(n==0){
-    return 1;//base case me return likhna mandatory hai since it returns the answer to the function
-
-}else{
-   return n*fact(n-1);
-
+    // printing power of a number,
 }
-
- //printing power of a number, 
-}
-double power(int n){
-    if(n==0){
+double power(int n)
+{
+    if (n == 0)
+    {
         return 1;
     }
-    else{
-        return 2*power(n-1);
+    else
+    {
+        return 2 * power(n - 1);
     }
 }
-//counting numbers tail recusrions
-void count(int n){
+// counting numbers tail recusrions
+void count(int n)
+{
 
-    if(n==0){
-        cout<<0;
-        return ;
-    }
-    else{
-        
-        cout<<n<<endl;
-       count(n-1);       //recursive relation
-       
-        
-    }
-
-}
-//void counting numbers head recurison
-
-void princount(int n){
-    if(n==0){       //base condition
-        cout<<0;
+    if (n == 0)
+    {
+        cout << 0;
         return;
     }
-    princount(n-1); //recursive relation
+    else
+    {
 
-    cout<<n<<endl;        //function body;
+        cout << n << endl;
+        count(n - 1); // recursive relation
+    }
+}
+// void counting numbers head recurison
 
+/*
+ αc∞♠EhxΣ
+n=3 ->bcc -> 2 -> 1 ->0
+
+
+
+void printcoutn
+
+
+
+
+
+
+
+
+*/
+
+void printcount(int n)
+{
+    // base case:
+    if (n == 1)
+    {
+        cout << n << endl;
+        return;
+    }
+    else
+    {
+        cout << n << endl;
+        n--;
+        printcount(n);
+    }
 }
 
-
-int main(){
-   //int ans  = power(3);
-   //cout<<ans;
+int recurscubesum(int N)
+{
+    if (N == 1)
+    {
+        return 0;
+    }
+    else
+    {
+        return (N - 1) * (N - 1) * (N - 1) + recurscubesum(N - 1);
+    }
+}
+int main()
+{
+    // int ans  = power(3);
+    // cout<<ans;
     int a = 10;
-    princount(500);
+    int c = recurscubesum(5);
+    cout << c;
     return 0;
 }
---
+++ recursionsandstrings.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // recursions and strings:
+    // i/p - string:abcde
+    // o/p - reverse:edcba
+
+    // without recursion:
+    //  string a = "SAKET";
+    //  int j = 4;
+
+    // int i = 0;
+    // while (i < j)
+    // {
+    //     swap(a[i], a[j]);
+    //     i++;
+    //     j--;
+    // }
+    // cout << a;
+
+    // using recursion:
+
+    return 0;
+}
